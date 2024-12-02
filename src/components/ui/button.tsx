60a2c869a9d2527bae47338d529652aa38dd032a
Nattakorn <nattkorn@fintech.co.th>
2024-12-02 23:02:20 +0700

    feat: add custom components

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,11 +13,12 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-success bg-background shadow-sm text-success hover:bg-success/10 hover:text-success-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-primary hover:text-primary-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        sidebar: "justify-start hover:bg-accent hover:text-accent-foreground font-normal"
       },
       size: {
         default: "h-9 px-4 py-2",
